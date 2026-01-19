865d097466cb959fca4478c3c9003e6a7f1b7c85
Alexander Gugger <agugger8315@floridapoly.edu>
2026-01-19 03:22:23 +0000

    1st Homework Commit

---
+++ print_test.py
@@ -0,0 +1,15 @@
+
+#Task 2
+my_name = "Alexander Gugger"
+
+#Task 3
+print(my_name)
+
+#Task 4
+print("PEP 8 guidelines on Code Lay-out say to use 4 spaces per indentation level")
+
+#Task 5
+print("Python uses identation to define code blocks instead of braces or keywords.")
+
+#Task 6
+print("I hope to become well versed in Python Programming, I hope to learn to be able to creatively solve prompts that require me to write code, and finally, I hope to learn to implement efficient solutions for code based assignments")
